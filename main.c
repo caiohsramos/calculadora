b40dc7ea60c0e3864453ef7d2b6bc357fc9096f5
martinelligor <igor.martinelli@usp.br>
2015-10-14 14:28:06 -0300

    Alteracoes na main

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char *argv[]) {
 	
+	/*Entao, quando abri o projeto tambem ja vi que era a notacao polonesa, meio adaptada por causa dos parenteses mas vi que era ela haha,
+	mas tambem acredito que a melhor alternativa seja uma pilha, inclusive se eu nao me engano eu tenho pronto um programa ja com calculadora
+	em notacao polonesa reversa, posso te enviar pra voce dar uma olhada, eh ate feito com pilha porque meu amigo teve esse trabalho e pra ajudar
+	ele eu mandei o de lab de icc do semestre passado ai quando ele terminou o com pilha ele me enviou pra eu ver, ai se voce achar que ajuda posso enviar
+	mas nao vou garantir que esta certo ou bonito porque nem olhei hahaha.*/
+
+	
 	/* Uma das ideias que tive pra esse trabalho era ler uma linha
 	 * 	inteira da conta e converter para "polonesa reversa". Desse
 	 * 	jeito, poderiamos usar o TAD Pilha para dar o resultado final
